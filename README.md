f773c445952321b9175e511465760ca478602028
Royendgel Silberie <royendgel@gmail.com>
2016-06-15 12:22:13 -0400

    code

--- README.md
+++ README.md
@@ -2,10 +2,17 @@
 
 #### One of my first attempts to make Rust work.
 
-This projects aims to read nginx logs and mirror it to a database using async and threads + Multiprocessors.
-I will use this for cambackup.com to see if a hls stream is active.
+This projects aims to read nginx logs and mirror it to a database using async and multi threads processors.
+I will use this for cambackup.com to check if a hls stream is active. (I use nginx-rtmp module)
 
-In nginx I have somethin like this :
+####Usage
+
+build it using `cargo build --release`
+- create an environment variable nginx_path like this `export nginx_path='path/to/your/access.log'`
+- run the executable like this `./path-to-executable/nglog`
+
+
+In nginx I have something like this :
 
 ```
 location /hls {
@@ -24,6 +31,6 @@ The log is in standard format.
 I fell in love with Rust language the same way I felt in love with Python some years ago, and
 this is my effort to learn it and do some advanced things like memory management, borrowing.
 Rust is a low level programming language that means it is fast (some claims faster then C++).
-I think to replace a lot of cambackup.com code with Rust.
+I have plan to migrate some heavy code that is written in Python on cambackup.com to Rust.
 
 Rust = Faster + less resource usage
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,63 @@
+extern crate postgres;
+extern crate getopts;
+use postgres::{Connection, SslMode};
+
+use std::io::BufReader;
+use std::io::prelude::*;
+use std::fs::File;
+use std::io;
+use std::path::Path;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+
+    let mut nginx_path = String::new();
+
+    for (key, value) in env::vars() {
+        if key == "nginx_path" {
+            nginx_path = value;
+            println!("The file path is {}", &nginx_path);
+        }
+    }
+    println!("{:?}", nginx_path);
+    let b: &Path = Path::new(&nginx_path);
+    let res = parse_log_file_by_line(&b);
+    // Check the result
+    match res {
+        Ok(_) => println!("Succesfully processed!"),
+        _ => println!("An Error ocured! : {:?}", res),
+    }
+
+    let db_res = db("wepa").execute("\\dt;", &[]);
+    println!("{:?}", db_res);
+
+
+    println!("Started !!");
+
+
+
+}
+
+// Reads log file line by line
+fn parse_log_file_by_line(path: &Path) -> Result<(), io::Error> {
+    // Opens the log file and read it, what then ?
+    println!("{:?}", path);
+    let file = try!(File::open(path));
+
+
+    let file = BufReader::new(&file);
+
+    for line in file.lines() {
+        println!("{:?}", line);
+    }
+
+    Ok(())
+}
+
+
+// Returns a db connection which we will use to store the log data
+fn db(connection: &'static str) -> postgres::Connection {
+
+    let conn = Connection::connect(connection, SslMode::None);
+    conn.unwrap()
 }
