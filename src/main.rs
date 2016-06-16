ecf612faf629495bda24c70919a50b5deb2033ec
Royendgel Silberie <royendgel@gmail.com>
2016-06-16 16:39:05 -0400

    Regex pattern and print results
    I created a new function extract_line, this function will take each line and process it (match=> regex)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 extern crate postgres;
 extern crate getopts;
+extern crate regex;
 use postgres::{Connection, SslMode};
 
 use std::io::BufReader;
@@ -8,6 +9,8 @@ use std::fs::File;
 use std::io;
 use std::path::Path;
 use std::env;
+
+use regex::Regex;
 
 fn main() {
 
@@ -28,11 +31,12 @@ fn main() {
         _ => println!("An Error ocured! : {:?}", res),
     }
 
-    let db_res = db("wepa").execute("\\dt;", &[]);
-    println!("{:?}", db_res);
-
-
-    println!("Started !!");
+    // let db_res = db("wepa").execute("\\dt;", &[]);
+    // println!("{:?}", db_res);
+    //
+    //
+    // println!("Started !!");
+    // // Test
 
 
 
@@ -44,11 +48,10 @@ fn parse_log_file_by_line(path: &Path) -> Result<(), io::Error> {
     println!("{:?}", path);
     let file = try!(File::open(path));
 
-
     let file = BufReader::new(&file);
 
     for line in file.lines() {
-        println!("{:?}", line);
+        extract_line(line.unwrap().as_str());
     }
 
     Ok(())
@@ -60,4 +63,31 @@ fn db(connection: &'static str) -> postgres::Connection {
 
     let conn = Connection::connect(connection, SslMode::None);
     conn.unwrap()
+}
+
+// processes line , maybe this function should call db ??
+fn extract_line(line: &str) -> &str {
+    // I'm depending on a space FIXME !!
+    let re =
+        Regex::new(r#"(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - - \[(.*)\] "(POST|GET) (.+)(HTTP.*?").(\d+).(\d+) (.*)"#)
+            .unwrap();
+    // println!("{:?}", re);
+    for cap in re.captures_iter(line) {
+        // println!("{:?}", line);
+        // println!("IP: {} Date: {} method: {} url: {}",
+        println!("IP: {} Date: {} method: {} url: {} request: {} status: {} body_bytes_sent: {} \
+                  http_referer: {} http_user_agent: {}",
+                 cap.at(1).unwrap_or(""),
+                 cap.at(2).unwrap_or(""),
+                 cap.at(3).unwrap_or(""),
+                 cap.at(4).unwrap_or(""),
+                 cap.at(5).unwrap_or(""),
+                 cap.at(6).unwrap_or(""),
+                 cap.at(7).unwrap_or(""),
+                 cap.at(8).unwrap_or(""),
+                 cap.at(9).unwrap_or(""));
+    }
+
+    "re.as_str()"
+
 }
